ec8f8cd1e074b26b0bff976d3053969ebe25e76f
Sheansuke <suerojean@gmail.com>
2022-07-23 15:52:13 -0400

    :sparkles: [FEAT]: added: toggle theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import { Keyboard } from "@organism/Keyboard";
 import ConfigTyping from "@molecules/ConfigTyping";
+import { ToggleTheme } from "@atoms/ToggleTheme";
 
 const Home: NextPage = () => {
   return (
     <>
+      <ToggleTheme listTheme={["dark", "light"]} />
       <ConfigTyping />
       <Keyboard />
     </>
